ab454113363219e642d8b96c682dd8fd2fc05643
idvele <ilari.katajamaki1@gmail.com>
2022-06-08 10:12:24 +0300

    first commit

---
+++ 8.6 Shoppinglist/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _8._6_Shoppinglist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
